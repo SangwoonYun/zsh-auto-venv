8065dc7820323f767b839273c515bcf93171bc41
Sangwoon Yun <dev.swyun@gmail.com>
2024-03-26 20:21:35 +0900

    fix: Modify last update check logic

--- zsh-auto-venv.plugin.zsh
+++ zsh-auto-venv.plugin.zsh
@@ -1,26 +1,40 @@
 # Plugin update check function
 auto_venv_update_check() {
-    local remote_tag local_tag repo_path user_input
-    repo_path="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-auto-venv"
-    
-    # Check if git is available, if not, skip update check
-    if ! command -v git &> /dev/null; then
-        # Skip the update check if git is not installed
+    local update_file="$ZSH_CUSTOM/plugins/zsh-auto-venv/.last_update_check"
+    local current_time=$(date +%s)
+    local last_update_check
+
+    if [[ -f "$update_file" ]]; then
+        last_update_check=$(cat "$update_file")
+    else
+        last_update_check=0
+    fi
+
+    # Check for updates every 24 hours (86400 seconds)
+    if (( current_time - last_update_check < 86400 )); then
+        # Last check was less than 24 hours ago, skip update check
         return
     fi
 
-    # Fetch the latest tags from the remote, skip if fails
+    # Record the time of this update check
+    echo $current_time > "$update_file"
+
+    # Proceed with the rest of the update check process...
+    local remote_tag local_tag repo_path user_input
+    repo_path="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-auto-venv"
+    
+    # Skip update check if git is not installed
+    if ! command -v git &> /dev/null; then return; fi
+    
+    # Skip if fails to fetch the latest tags from the remote
     git -C "$repo_path" fetch --tags 2> /dev/null || return
     
-    # Get the latest tag name from the remote and local, continue only if both are retrieved
+    # Continue only if both tags are retrieved successfully
     remote_tag=$(git -C "$repo_path" describe --tags `git -C "$repo_path" rev-list --tags --max-count=1` 2> /dev/null)
     local_tag=$(git -C "$repo_path" describe --tags 2> /dev/null)
-    if [ -z "$remote_tag" ] || [ -z "$local_tag" ]; then
-        # Skip the update process if unable to retrieve tags
-        return
-    fi
+    if [ -z "$remote_tag" ] || [ -z "$local_tag" ]; then return; fi
     
-    # Check if the latest tag matches the currently checked-out version
+    # Notify user of new version and ask for update permission
     if [ "$remote_tag" != "$local_tag" ]; then
         echo "New version available for zsh-auto-venv: $remote_tag (current version: $local_tag)"
         echo -n "Do you want to update the plugin now? [Y/n] "
